3a84c7c132138f273f25da2b9fdbb05e16d4be24
noellerjd <noellerninja@gmail.com>
2022-10-20 21:16:17 -0600

    1.0 build

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # JS-Time-Controls
+
+## Description
+
+My motivation behind this project was to get some more experience using the built in Date function with Javascript.
+
+I built this project to allow the user to have control over showing the date and to change the format of the time.
+
+I learned a lot about formatting and toggling a function using JS.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+If you want to install you can clone the repository. No further installation necessary, it should work upon launching the index.html
+
+## Usage
+
+In order to toggle the calender, push the calender icon on the left.
+
+In order to toggle the time format, push the clock icon on the left.
+
+## License
+
+None
+
+### Created By:
+
+[Justin Noeller](https://github.com/noellerjd)
---
+++ script.js
@@ -0,0 +1,109 @@
+const clockBtn = document.getElementById("clock-btn");
+// const clockToggle = document.getElementsByClassName("clock");
+
+const clockAMPM = document.getElementById("AMPM");
+const clock24h = document.getElementById("24h");
+
+var t1;
+var t2;
+var d1;
+
+function currentDate() {
+  d1 = setInterval(function () {
+    let date = new Date();
+    let weekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let dateNow = date.toLocaleDateString();
+    let day = weekday[date.getDay()];
+
+    document.getElementById("date").innerHTML = day + " " + dateNow;
+  }, 1000);
+}
+
+// Time Toggles
+function toggleAMPM() {
+  if (document.getElementById("time").className === "AMPM") {
+    t1 = setInterval(function () {
+      let date = new Date();
+      let hour = date.getHours();
+      let minute = date.getMinutes();
+      let seconds = date.getSeconds();
+      let ampm = "AM";
+
+      if ((hour) => 12) {
+        hour = hour - 12;
+        ampm = "PM";
+      }
+
+      if (minute < 10) {
+        minute = "0" + minute;
+      }
+
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      document.getElementById("time").innerHTML =
+        hour + ":" + minute + ":" + seconds + " " + ampm;
+    }, 1000);
+  }
+  document.getElementById("time").className = "AMPM";
+}
+
+function toggle24h() {
+  if (document.getElementById("time").className === "24hr") {
+    t2 = setInterval(function () {
+      let date = new Date();
+      let hour = date.getHours();
+      let minute = date.getMinutes();
+      let seconds = date.getSeconds();
+
+      if (minute < 10) {
+        minute = "0" + minute;
+      }
+
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      document.getElementById("time").innerHTML =
+        hour + ":" + minute + ":" + seconds;
+    }, 1000);
+  }
+}
+
+// calender toggles
+
+function clockToggle() {
+  if (document.getElementById("time").className === "AMPM") {
+    clearInterval(t1);
+    document.getElementById("time").className = "24hr";
+    toggle24h();
+  } else {
+    clearInterval(t2);
+    document.getElementById("time").className = "AMPM";
+    toggleAMPM();
+  }
+}
+
+function calenderToggle() {
+  if (document.getElementById("date").className === "show-date") {
+    clearInterval(d1);
+    document.getElementById("date").className = "hide-date";
+    document.getElementById("date").style.display = "none";
+  } else if (document.getElementById("date").className === "hide-date") {
+    currentDate();
+    document.getElementById("date").className = "show-date";
+    document.getElementById("date").style.display = "block";
+  }
+}
+
+clockToggle();
+currentDate();
